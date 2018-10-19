17862d68d31fbd25984ce67ebcf1f85158aaf0f1
deonara447 <deonara447@ed.amdsb.ca>
2018-10-19 14:15:43 -0400

    Add project files.

---
+++ GuessingGameFormative/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace GuessingGameFormative
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.intructionsLabel = new System.Windows.Forms.Label();
+            this.numberInput = new System.Windows.Forms.TextBox();
+            this.guessButton = new System.Windows.Forms.Button();
+            this.hintLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 30.875F);
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(80, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(658, 99);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Guessing Game";
+            // 
+            // intructionsLabel
+            // 
+            this.intructionsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.875F);
+            this.intructionsLabel.ForeColor = System.Drawing.Color.White;
+            this.intructionsLabel.Location = new System.Drawing.Point(56, 108);
+            this.intructionsLabel.Name = "intructionsLabel";
+            this.intructionsLabel.Size = new System.Drawing.Size(695, 46);
+            this.intructionsLabel.TabIndex = 1;
+            this.intructionsLabel.Text = "Enter a number between 1 and 100";
+            // 
+            // numberInput
+            // 
+            this.numberInput.Location = new System.Drawing.Point(348, 190);
+            this.numberInput.Name = "numberInput";
+            this.numberInput.Size = new System.Drawing.Size(100, 31);
+            this.numberInput.TabIndex = 2;
+            // 
+            // guessButton
+            // 
+            this.guessButton.Location = new System.Drawing.Point(302, 241);
+            this.guessButton.Name = "guessButton";
+            this.guessButton.Size = new System.Drawing.Size(201, 64);
+            this.guessButton.TabIndex = 3;
+            this.guessButton.Text = "Guess";
+            this.guessButton.UseVisualStyleBackColor = true;
+            this.guessButton.Click += new System.EventHandler(this.guessButton_Click);
+            // 
+            // hintLabel
+            // 
+            this.hintLabel.ForeColor = System.Drawing.Color.White;
+            this.hintLabel.Location = new System.Drawing.Point(225, 358);
+            this.hintLabel.Name = "hintLabel";
+            this.hintLabel.Size = new System.Drawing.Size(333, 42);
+            this.hintLabel.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.hintLabel);
+            this.Controls.Add(this.guessButton);
+            this.Controls.Add(this.numberInput);
+            this.Controls.Add(this.intructionsLabel);
+            this.Controls.Add(this.titleLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label intructionsLabel;
+        private System.Windows.Forms.TextBox numberInput;
+        private System.Windows.Forms.Button guessButton;
+        private System.Windows.Forms.Label hintLabel;
+    }
+}
+
---
+++ GuessingGameFormative/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessingGameFormative
+{
+    public partial class Form1 : Form
+    {
+        Random randGen = new Random();
+        int playerGuess;
+        int correctNumber;
+        int difference;
+        int numAbsolute
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            correctNumber = randGen.Next(1, 101);
+        }
+
+        private void guessButton_Click(object sender, EventArgs e)
+        {
+            
+            playerGuess = Convert.ToInt32(numberInput.Text);
+            //difference = correctNumber - playerGuess;
+            //numAbsolute = Math.Abs(difference);
+
+
+            if (playerGuess < correctNumber)
+            {
+                hintLabel.Text = "Too Low";
+            }
+
+            if (playerGuess > correctNumber)
+            {
+                hintLabel.Text = "Too High";
+            }
+
+            if (playerGuess == correctNumber)
+            {
+                hintLabel.Text = "You got it!";
+            }
+
+        }
+    }
+}
